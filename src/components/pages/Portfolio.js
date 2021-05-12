1cab968b0325a19c9a9e94ba9c81998bb2b4595e
BhuvaneshRavi <bhuvanesh7394@gmail.com>
2021-05-11 21:30:32 -0400

    social media links

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import '../../App.css';
 import Typed from 'react-typed';
-import Particles from "react-particles-js"
-import introImg from '../../Assets/images/img-intro.jpg'
+import Particles from "react-particles-js";
+import introImg from '../../Assets/images/img-intro.jpg';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faInstagram} from '@fortawesome/free-brands-svg-icons';
+import {faSoundcloud} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Link} from 'react-router-dom';
 
 function Portfolio() {
     // return <h1 className='portfolio'>Portfolio</h1>;
@@ -39,7 +46,7 @@ function Portfolio() {
             <div className="introRight">
                 <h2> Hi there, I'm </h2>
                 <h1>Bhuvaneshwaran Ravi</h1>
-                <h2> Software Developer</h2>
+                <h2> Software Engineer</h2>
                 <Typed 
                 className='typed-text' 
                 strings={["Eat()","Code()","Sleep()","Repeat()"]}
@@ -48,6 +55,23 @@ function Portfolio() {
                 backSpeed={10}
                 loop
                 />
+                <div className='icons'>
+                    <Link to={{ pathname: "https://www.facebook.com/haibhuvan" }} target="_blank" className='icon-holder'> 
+                        <FontAwesomeIcon icon={faFacebook} className='icon-fb'/>
+                    </Link>
+                    <Link to={{ pathname: "https://github.com/BhuvaneshRavi" }} target="_blank" className='icon-holder'>
+                        <FontAwesomeIcon icon={faGithub} className='icon-gh'/>
+                    </Link>
+                    <Link to={{ pathname: "https://linkedin.com/in/bhuvaneshwaran-ravi-9a5490116" }} target="_blank" className='icon-holder'>
+                        <FontAwesomeIcon icon={faLinkedin} className='icon-li'/>
+                    </Link>
+                    <Link to={{ pathname: "https://soundcloud.com/bhuvaneshwaran-ravi/sets/a-melomaniacs-list" }} target="_blank" className='icon-holder'>
+                        <FontAwesomeIcon icon={faSoundcloud} className='icon-sc'/>
+                    </Link>
+                    <Link to={{ pathname: "https://www.instagram.com/bhuvanesh_ravi" }} target="_blank" className='icon-holder'>
+                        <FontAwesomeIcon icon={faInstagram} className='icon-ig'/>
+                    </Link>
+                </div>
             </div>
         </div>
     </div>
